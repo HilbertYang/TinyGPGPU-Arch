24aa46518197c625f20d194a363389c9632655b6
Hilbert Yang <hilbert_yang@outlook.com>
2026-02-27 19:56:51 -0800

    Update sample

--- compiler/gpu_program_sample_for_mul.asm
+++ compiler/gpu_program_sample_for_mul.asm
@@ -4,10 +4,10 @@
     1   LD_PARAM    R2      0       0       0      //load b
     2   LD_PARAM    R3      0       0       0      //load c/out
     3   LD_PARAM    R4      0       0       0      //load n
-    4   ADDIMM      R5      R0      0       0      //reset thread_id(R5=R0(0)+0)
+    4   MOV         R5      0       0       0      //reset thread_id(R5=0)
 Loop:
     5   SETP_GE     0       R5      R4      0       //compare
-    6   BRA         0       0       0       RET     //if thread_id > n end the kernel
+    6   BPR         0       0       0       RET     //if thread_id > n end the kernel
 
     7   SHIFTRV     R6      R5      0       2        //TID/4
     8   ADD64       R1      R1      R6      0
@@ -20,6 +20,6 @@ Loop:
 
     e   ST64        R12     R3      0       0
     f   ADDIMM      R5      R5      0       4
-    10  J           0       0       0       Loop
+    10  BR          0       0       0       Loop
 RET:
     11  RET         0       0       0       0     //DONE CALCULATION
