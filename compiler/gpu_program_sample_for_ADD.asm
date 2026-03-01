1752c4aea293f6a08e9e8a777e3bcc1d7e357997
HilbertYang <hilbert_yang@outlook.com>
2026-02-28 22:41:30 -0800

    Update gpu_prog_sample_for_ADD.asm

--- compiler/gpu_program_sample_for_ADD.asm
+++ compiler/gpu_program_sample_for_ADD.asm
@@ -11,15 +11,15 @@ Loop:
     7   SETP_GE     0       R5      R4      0       //compare
     8   BPR         0       0       0       RET     //if thread_id > n end the kernel
         
-    15  LD64        R10     R1      0       0      // R10 = MEM[R1]
-    16  LD64        R11     R2      0       0      // R11 = MEM[R2]
-    12  ADDI64      R1      R1      0       1      // R1 += 1;
-    13  ADDI64      R2      R2      0       1      // R2 += 1;
-    19  ADD_I16     R12     R10     R11     0      // R12 = R11 + R10
-    f   BR          0       0       0       Loop
-    e   ADDI64      R5      R5      0       4      // R5 += 4;
-    d   ST64        R12     R3      0       0      // MEM[R3] = R12(result)
-    14  ADDI64      R3      R3      0       1      // R3 += 1;
+    9   LD64        R10     R1      0       0      // R10 = MEM[R1]
+    10  LD64        R11     R2      0       0      // R11 = MEM[R2]
+    11  ADDI64      R1      R1      0       1      // R1 += 1;
+    12  ADDI64      R2      R2      0       1      // R2 += 1;
+    13  ADD_I16     R12     R10     R11     0      // R12 = R11 + R10
+    14  BR          0       0       0       Loop
+    15  ADDI64      R5      R5      0       4      // R5 += 4;
+    16  ST64        R12     R3      0       0      // MEM[R3] = R12(result)
+    17  ADDI64      R3      R3      0       1      // R3 += 1;
 
 RET:
-    10  RET         0       0       0       0     //DONE CALCULATION
+    18  RET         0       0       0       0     //DONE CALCULATION
